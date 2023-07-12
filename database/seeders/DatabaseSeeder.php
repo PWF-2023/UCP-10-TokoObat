1ff43e1f5c2dc876bc821bfee66d57c7c1fd7242
AskarKadir <askarkadir07@gmail.com>
2023-07-12 20:18:39 +0700

    seeder & table user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name'     => 'Admin',
+            'email'    => 'admin@example.com',
+            'password' => bcrypt('password'),
+            // password
+            'is_admin' => true,
+        ]);
+
+        User::create([
+            'name'     => 'User',
+            'email'    => 'user@example.com',
+            'password' => bcrypt('password'),
+            // password
+            'is_admin' => false,
+        ]);
     }
 }
