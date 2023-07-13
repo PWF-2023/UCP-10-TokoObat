b92fdcd85d49a9d3dd9cb0103570273a647d05d4
AsraSyahrastani <asra.s.ft20@mail.umy.ac.id>
2023-07-13 22:18:45 +0700

    transaksi controller,model,dan table

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'nama_costumer',
+        'jumlah_item',
+        'total_item',
+        'is_available',
+    ];
+
+    protected $casts = [
+        'is_available' => 'boolean',
+    ];
+
+}
