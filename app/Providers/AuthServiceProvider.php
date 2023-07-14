50f6a6b06f7d27134b06ec5cf428970c0bd8678e
AskarKadir <askarkadir07@gmail.com>
2023-07-14 09:19:02 +0700

    cashier nav fix

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -22,7 +23,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('isAdmin', function ($user) {
+        Gate::define('admin', function (User $user) {
             return $user->is_admin == true;
         });
     }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    {{-- <x-nav-link :href="route('cashier.index')" :active="request()->routeIs('cahiser.index')">
+                        {{ __('Cashier') }}
+                    </x-nav-link> --}}
                     @can('admin')
                         <x-nav-link :href="route('cashier.index')" :active="request()->routeIs('cashier.*')">
                             {{ __('Cashier') }}
