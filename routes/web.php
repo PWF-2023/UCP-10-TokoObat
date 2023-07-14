3dac0f2ed4bf16d9b374d3dd7ae8d2c2a16a55fb
AskarKadir <askarkadir07@gmail.com>
2023-07-14 16:20:27 +0700

    fixing darkmode

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\BarangController;
-use App\Http\Controllers\CashierController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -28,27 +23,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    // profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Barang
-    Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
-    Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
-    Route::post('/barang', [BarangController::class, 'store'])->name('barang.store');
-    Route::get('/barang/{barang}', [BarangController::class, 'show'])->name('barang.show');
-    Route::get('/barang/{barang}/edit', [BarangController::class, 'edit'])->name('barang.edit');
-    Route::put('/barang/{barang}', [BarangController::class, 'update'])->name('barang.update');
-    Route::delete('/barang/{barang}', [BarangController::class, 'destroy'])->name('barang.destroy');
-
-    // cashier
-    Route::middleware('admin')->group(function () {
-        Route::get('/cashier', [CashierController::class, 'index'])->name('cashier.index');
-        Route::delete('/cashier/{cashier}', [CashierController::class, 'destroy'])->name('cashier.destroy');
-        Route::patch('/cashier/{cashier}/makeadmin', [CashierController::class, 'makeadmin'])->name('cashier.makeadmin');
-        Route::patch('/cashier/{cashier}/removeadmin', [CashierController::class, 'removeadmin'])->name('cashier.removeadmin');
-    });
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
